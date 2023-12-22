d66259d35a52d7b10c0ef3febcbb3ad8d52d34f1
romain isoz <isozromain@gmail.com>
2023-12-22 22:38:02 +0100

    test user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :age, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the truth' do
+    assert true
+  end
+
+  test 'should not save user without email' do
+    user = User.new(age: 20, username: 'test', password: 'test')
+    assert_not user.save
+  end
+
+  test 'should not save user without age' do
+    user = User.new(email: 'test@gmail.com', username: 'test', password: 'test')
+    assert_not user.save
+  end
+
+  test 'should not save user without username' do
+    user = User.new(email: 'test@gmail.com', age: 20, password: 'test')
+    assert_not user.save
+  end
+
+  test 'should not save user without password' do
+    user = User.new(email: 'test@gmail.com', age: 20, username: 'test')
+    assert_not user.save
+  end
 end

6fd828e227d18d7ac2693d505555ff143e4bdec8
romain isoz <isozromain@gmail.com>
2023-12-22 23:01:51 +0100

    add users controller and modify sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+
+  # LOGGING IN
+  def create
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }
+    else
+      render json: { error: 'Invalid username or password' }
+    end
+  end
+
+  def auto_login
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :age)
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  # Register
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }
+    else
+      render json: { error: 'Can not Create the User' }
+    end
+  end
+end

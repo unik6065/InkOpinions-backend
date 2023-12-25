b071038190f1a6f6307c4296a7896d85f35c51e8
romain isoz <isozromain@gmail.com>
2023-12-25 11:54:52 +0100

    test user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   # Register
   def create
     @user = User.create(user_params)
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should create a user when ask for it' do
-    assert true
+    post '/user', params: { username: 'john', password: 'test', age: 23, email: 'john@doe.com' }
+    assert_response :success
+    assert_equal 'john', JSON.parse(@response.body)['user']['username']
+  end
+
+  test 'should not create a user if not all params are fulfilled' do
+    post '/user', params: { username: 'john' }
+    assert_equal 'Can not Create the User', JSON.parse(@response.body)['error']
   end
 end

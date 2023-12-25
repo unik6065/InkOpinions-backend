f40fd314e2d2aedc5486ad53e3da64020b0faeda
romain isoz <isozromain@gmail.com>
2023-12-25 12:28:35 +0100

    test book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = Book.new(name: 'lotr', description: 'Lorem.ipsum', weight: 25)
+  end
+
+  test 'should not create a book if name is nil' do
+    @book.name = nil
+    assert_not @book.valid?
+  end
+
+  test 'should not create a book if weight is nil' do
+    @book.weight = nil
+    assert_not @book.valid?
+  end
+
+  test 'should create a book if everything is fulfilled' do
+    assert @book.valid?
+  end
 end

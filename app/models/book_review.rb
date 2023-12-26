abd8275eddaa49c29f13b47e78b26f14d260898c
romain isoz <isozromain@gmail.com>
2023-12-26 22:41:52 +0100

    create book_review and test it

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -1,2 +1,9 @@
 class BookReview < ApplicationRecord
+  has_one :user
+  has_one :book
+
+  validates :grade, presence: true
+  validates :grade, numericality: { greater_than: 0, less_than: 6 }
+  validates :user_id, presence: true
+  validates :book_id, presence: true
 end
--- test/models/book_review_test.rb
+++ test/models/book_review_test.rb
@@ -1,7 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class BookReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @lotr_review = BookReview.new(grade: 5, review: 'best book ever', user_id: users(:test_user).id,
+                                  book_id: books(:lotr).id)
+  end
+
+  test 'a Book Review must be saved if everything is fine' do
+    assert @lotr_review.valid?
+  end
+
+  test 'a Book Review must not be saved if there is no user' do
+    @lotr_review.user_id = nil
+    assert_not @lotr_review.valid?
+  end
+
+  test 'a Book review must not be saved if there is no book' do
+    @lotr_review.book_id = nil
+    assert_not @lotr_review.valid?
+  end
+
+  test 'a Book review must have a grade' do
+    @lotr_review.grade = nil
+    assert_not @lotr_review.valid?
+  end
+
+  test 'a grade must smaller than 5' do
+    @lotr_review.grade = 6
+    assert_not @lotr_review.valid?
+  end
+
+  test 'a grade must be greater than 0' do
+    @lotr_review.grade = 0
+    assert_not @lotr_review.valid?
+  end
 end
